f5f60ff8d260f296b749c6a0bed7b3aa492eb4b0
Dao Pham <daopm.dean@gmail.com>
2022-06-25 16:43:37 +0700

    update release

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Tap Counter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
